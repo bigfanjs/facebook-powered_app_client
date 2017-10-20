724c906db09255cfcfe1223c38c308eb5723ab6c
Adel Jojo <bigfanjs@gmail.com>
2017-10-20 12:23:18 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">So Far So Good</div>
+    );
+  }
+}
+
+export default App;
