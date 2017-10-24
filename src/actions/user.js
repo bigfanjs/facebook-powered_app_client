5ba9fcc28800887425e7362d09b9a59ca1ce089a
Adel Jojo <bigfanjs@gmail.com>
2017-10-24 11:22:44 +0100

    feat: creates async signup user action creator

--- src/actions/user.js
+++ src/actions/user.js
@@ -2,6 +2,10 @@ export const SIGNIN_USER = 'SIGNIN_USER';
 export const SIGNIN_USER_SUCCESS = 'SIGNIN_USER_SUCCESS';
 export const SIGNIN_USER_FAILURE = 'SIGNIN_USER_FAILURE';
 
+export const SIGNUP_USER = 'SIGNUP_USER';
+export const SIGNUP_USER_SUCCESS = 'SIGNUP_USER_SUCCESS';
+export const SIGNUP_USER_FAILURE = 'SIGNUP_USER_FAILURE';
+
 function signinUserRequest() {
   return {
     type: SIGNIN_USER
@@ -38,6 +42,52 @@ export function signinUser(email, password) {
       })
       .catch((error) => {
         dispatch(signinUserFailure(error));
+      });
+  
+    dispatch();
+  };
+}
+
+function signupUserRequest() {
+  return {
+    type: SIGNUP_USER
+  };
+}
+
+function signupUserSuccess() {
+  return {
+    type: SIGNUP_USER_SUCCESS
+  };
+}
+
+function signupUserFailure(error) {
+  return {
+    type: SIGNUP_USER_FAILURE,
+    error: error
+  };
+}
+
+export function signupUser(email, password) {
+  const config = {
+    method: 'post',
+    headers: { 'Content-Type':'application/x-www-form-urlencoded' },
+    body: `email=${ email }&password=${ password }`
+  };
+
+  return (dispatch) => {
+    dispatch(signupUserRequest());
+    fetch('/api/users/signup', config)
+      .then((res) => res.json())
+      .then((user) => {
+        if (!user.response.ok) {
+          dispatch(signupUserFailure(user.message));
+        } else {
+          localStorage.setItem('jwt', user.data.token);
+          dispatch(signupUserSuccess());
+        }
+      })
+      .catch((error) => {
+        console.error(error);
       });
   
     dispatch();
