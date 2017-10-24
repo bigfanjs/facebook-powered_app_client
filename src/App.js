d0caf72635c85f457d575be114a90c82a496bc00
Adel Jojo <bigfanjs@gmail.com>
2017-10-24 11:36:55 +0100

    feat: creates a public lazily loaded signin page

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import PrivateRoute from './components/private-route';
 import './App.css';
 
 const Home = Async(() => import('./scenes/home'));
+const Signin = Async(() => import('./scenes/signin'));
 
 class App extends Component {
   render() {
@@ -14,6 +15,7 @@ class App extends Component {
       <Router>
         <div>
           <PrivateRoute exact path="/" component={Home} />
+          <Route path="/signin" component={Signin} />
         </div>
       </Router>
     );
---
+++ src/scenes/signin/signin.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class componentName extends Component {
+  render () {
+    return (
+      <div>
+        Sginin Component
+      </div>
+    );
+  }
+}
+
+export default componentName;
