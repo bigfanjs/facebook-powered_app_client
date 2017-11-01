585f4a042aa3063b156309ce75aaa75f5e7ad797
Adel Jojo <bigfanjs@gmail.com>
2017-11-01 10:50:24 +0000

    feat: sets up the facebook sdk outside the component tree

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
-/* globals FB */
-
 import React, { Component } from 'react';
 import {connect} from 'react-redux';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 import Async from './components/async-component';
 import PrivateRoute from './components/private-route';
-import {getLoginStatus} from './actions/fb-login-status';
 
 import './App.css';
 
@@ -15,27 +12,6 @@ const Signin = Async(() => import('./scenes/signin'));
 const Signup = Async(() => import('./scenes/signup'));
 
 class App extends Component {
-  componentWillMount() {
-    window.fbAsyncInit = () => {
-      FB.init({
-        appId            : '325131021294419',
-        autoLogAppEvents : true,
-        xfbml            : true,
-        version          : 'v2.10'
-      });
-
-      this.props.dispatch(getLoginStatus());
-    };
-
-    (function(d, s, id){
-      var js, fjs = d.getElementsByTagName(s)[0];
-      if (d.getElementById(id)) {return;}
-      js = d.createElement(s); js.id = id;
-      js.src = 'https://connect.facebook.net/en_US/sdk.js';
-      fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));
-  }
-
   render() {
     const {verifying, loading} = this.props;
 
--- src/index.js
+++ src/index.js
@@ -4,10 +4,11 @@ import {Provider} from 'react-redux';
 import {createStore, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
 
-import registerServiceWorker from './registerServiceWorker';
 import App from './App';
 import FbPoweredApp from './reducers';
 import {VerifyUser} from './actions/user';
+import registerServiceWorker from './registerServiceWorker';
+import setupfacebookSDK from './setup-fb-sdk';
 
 import './index.css';
 
@@ -15,6 +16,7 @@ const store = createStore(FbPoweredApp, applyMiddleware(thunk));
 if (localStorage.jwt) {
   store.dispatch(VerifyUser(localStorage.jwt));
 }
+setupfacebookSDK(store);
 
 ReactDOM.render(
   <Provider store={store}>
